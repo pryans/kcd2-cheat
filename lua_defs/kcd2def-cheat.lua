2dcf7d8c958e31e0806549903718850a3aef5657
Andre Louis Issa <andrelouisissa@gmail.com>
2025-02-10 01:09:57 +1100

    reorganise lua defs

---
+++ lua_defs/kcd2def-cheat.lua
@@ -0,0 +1,32 @@
+---@meta kcd2def-cheat
+
+--- definition file containing LuaCATS annotations for the additions to the lua state after all modules of kcd2-cheat have loaded
+
+
+--- Common Structures (no metatable, but common fields)
+
+
+
+
+
+--- Actual Classes (have a metatable)
+
+
+
+
+
+--- Type Coercions (must use ---@type at consumer)
+
+
+
+
+
+--- Singleton Types (mainly for the fields of nested global tables)
+
+
+
+
+
+--- Global Annotations
+
+
--- lua_defs/kcd2def.lua
+++ lua_defs/kcd2def.lua
@@ -1,5 +1,12 @@
 ---@meta kcd2def
 
+--- definition file containing LuaCATS annotations for the lua state after all typical packages of Kingdom Come Deliverance II have loaded
+--- if a parameter is annotated starting with 'unk_', the purpose or type of that parameter is merely being guessed
+
+
+--- Common Structures (no metatable, but common fields)
+
+
 ---@class kcd2def*Vector
 ---@field public x number
 ---@field public y number
@@ -8,19 +15,25 @@
 ---@class kdc2def*Actor.PhysicalStats
 ---@field public mass number
 
+---@class kcd2def*Database.TableInfo
+---@field public LineCount integer
+
+---@class kcd2def*Database.TableLine: table
+
+
+--- Actual Classes (have a metatable)
+
+
 ---@class kcd2def*Actor
----@field public GetWorldPos fun(self:kcd2def*Actor): kcd2def*Vector
----@field public GetWorldDir fun(self:kcd2def*Actor): kcd2def*Vector
----@field public AwakePhysics fun(self:kcd2def*Actor, unk_1:1|number): nil
----@field public AddImpulse fun(self:kcd2def*Actor, unk_1:-1|number, unk_position:kcd2def*Vector, unk_up:kcd2def*Vector, unk_acceleration:number): nil
+---@field public GetWorldPos fun(self: kcd2def*Actor): kcd2def*Vector
+---@field public GetWorldDir fun(self: kcd2def*Actor): kcd2def*Vector
+---@field public AwakePhysics fun(self: kcd2def*Actor, unk_1: 1|number): nil
+---@field public AddImpulse fun(self: kcd2def*Actor, unk_1: -1|number, unk_position: kcd2def*Vector, unk_up: kcd2def*Vector, unk_acceleration: number): nil
 ---@field public GetPhysicalStats fun(self:kcd2def*Actor): kdc2def*Actor.PhysicalStats
 
----@class kcd2def*Actor-player:kcd2def*Actor
 
----@class kcd2def*Database.TableInfo
----@field public lineCount integer
+--- Type Coercions (must use ---@type at consumer)
 
----@class kcd2def*Database.TableLine:table
 
 ---@class kcd2def*Database.TableLine-quest:kcd2def*Database.TableLine
 ---@field public quest_id ...
@@ -31,12 +44,22 @@
 ---@field public objective_id ...
 ---@field public objective_name string
 
+
+--- Singleton Types (mainly for the fields of nested global tables)
+
+
+---@class kcd2def*Actor-player:kcd2def*Actor
+
+---@class kcd2def*Database
+---@field public GetTableInfo fun(tableName: string): kcd2def*Database.TableInfo
+---@field public GetTableLine fun(tableName: string, lineIndex: integer): kcd2def*Database.TableLine?
+
+
+--- Global Annotations
+
 ---@type kcd2def*Actor-player
 player = ...
 
----@type fun(tableName:string): kcd2def*Database.TableInfo
-Database.GetTableInfo = function(...) end
-
----@type fun(tableName:string, lineIndex:integer): kcd2def*Database.TableLine?
-Database.GetTableLine = function(...) end
+---@type kcd2def*Database
+Database = ...
 
