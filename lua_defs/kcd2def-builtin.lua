56ae4df585bc7e63c051647c65b8dae9f4c465a0
Andre Louis Issa <andrelouisissa@gmail.com>
2025-02-10 20:32:36 +1100

    added sources for altered builtins defs

--- lua_defs/kcd2def-builtin.lua
+++ lua_defs/kcd2def-builtin.lua
@@ -6,27 +6,36 @@
 
 
 math = {
+	---@source Data/Scripts/Utils/MathUtils.lua:58
 	---@nodiscard
 	---@type fun(n: number, min: number, max: number): number
 	Clamp = ...;
+	---@source Data/Scripts/Utils/MathUtils.lua:58
 	---@nodiscard
 	---@type fun(m: number, n: number, k: number): number
 	Lerp = ...;
+	--- Calculates almost-gauss distribution (omits the divisor, as results of this function will be used as weights in random sampling, therefore scaling is irrelevant )
+	---@source Data/Scripts/Utils/MathUtils.lua:89
 	---@nodiscard
 	---@type fun(m: number, o: number, x:number): number
 	PseudoGaussPDF = ...;
+	---@source Data/Scripts/Utils/MathUtils.lua:75
 	---@nodiscard
 	---@type fun(a: number, b: number): number
 	RandomFloat = ...;
+	---@source Data/Scripts/Utils/MathUtils.lua:69
 	---@nodiscard
 	---@type fun(num: number, index: number?): number
 	Round = ...;
+	---@source Data/Scripts/Utils/MathUtils.lua:64
 	---@nodiscard
 	---@type fun(n: number): 0|1|-1
 	Sign = ...;
+	---@source Data/Scripts/Utils/MathUtils.lua:94
 	---@nodiscard
 	---@type fun(m: number, o: number, s:number, x:number): number
 	SkewedPseudoGauss = ...;
+	---@source Data/Scripts/Utils/MathUtils.lua:48
 	---@nodiscard
 	---@type fun(a: number): number
 	Sqr = ...;
@@ -69,21 +78,27 @@ os = { -- reduced functions available
 }
 
 string = {
+	---@source Data/Scripts/Utils/StringUtils.lua:17
 	---@nodiscard
 	---@type fun(s: string): string
 	CapitaliseFirst = ...;
+	---@source Data/Scripts/Utils/StringUtils.lua:1
 	---@nodiscard
 	---@type fun(s: string, delim: string): string[]
 	Explode = ...;
+	---@source Data/Scripts/Utils/StringUtils.lua:50
 	---@nodiscard
 	---@type fun(str: string?): boolean
 	IsEmpty = ...;
+	---@source Data/Scripts/Utils/StringUtils.lua:28
 	---@nodiscard
 	---@type fun(str: string, delimiter: string?, omitEmpty: boolean?): string[]
 	Split = ...;
+	---@source Data/Scripts/Utils/StringUtils.lua:23
 	---@nodiscard
 	---@type fun(s: string, start: string): boolean
 	StartsWith = ...;
+	---@source Data/Scripts/Utils/StringUtils.lua:11
 	---@nodiscard
 	---@type fun(s: string): string
 	Trim = ...;
@@ -107,37 +122,46 @@ string = {
 }
 
 table = {
+	---@source Data/Scripts/Utils/TableUtils.lua:237
 	---@generic V: any
 	---@type fun(t1: V[], t2: V[])
 	Append = ...;
+	---@source Data/Scripts/Utils/TableUtils.lua:256
 	---@generic V: any
 	---@type fun(t1: {[kcd2def*cppindex]:V}, t2: {[kcd2def*cppindex]:V}?)
 	ArrayShiftOne = ...;
+	---@source Data/Scripts/Utils/TableUtils.lua:244
 	---@generic V: any
 	---@type fun(t1: V[], t2: V[]?)
 	ArrayShiftZero = ...;
 	--- Returns a boolean value based on whether
 	-- the provided table contains the provided value.
+	---@source Data/Scripts/Utils/TableUtils.lua:14
 	---@generic V: any
 	---@nodiscard
 	---@type fun(t: {[kcd2def*luakey]: V}, val: V): boolean
 	Contains = ...;
+	---@source Data/Scripts/Utils/TableUtils.lua:19
 	---@nodiscard
 	---@type fun(t: {[kcd2def*luakey]: string}, val: string): boolean
 	ContainsIgnoreCase = ...;
+	---@source Data/Scripts/Utils/TableUtils.lua:151
 	---@nodiscard
 	---@type fun(_tbl: kcd2def*tablelike?): integer
 	Count = ...;
+	---@source Data/Scripts/Utils/TableUtils.lua:164
 	---@generic V: any
 	---@nodiscard
 	---@type fun(t: {[kcd2def*luakey]: V}, pred: fun(v: V): boolean?): integer
 	CountWithPredicate = ...;
 	--- Creates a deep copy of a table. Handles recursive tables.
+	---@source Data/Scripts/Utils/TableUtils.lua:42
 	---@generic V: any
 	---@nodiscard
 	---@type fun(obj: V): V
 	DeepCopy = ...;
 	--- Find a value in a table. Returns its index if found and nil if not.
+	---@source Data/Scripts/Utils/TableUtils.lua:192
 	---@generic K: kcd2def*luakey
 	---@generic V: any
 	---@nodiscard
@@ -148,63 +172,76 @@ table = {
 	-- the value of these keys is a simple indexed array of all objects, that have according indexKey value
 	-- (e.g. for input array { {ik="A", val="a1"}, {ik="B", val="b1"}, {ik="B", val="b2"} }
 	-- and indexKey parameter "ik" returns { A = { {ik="A", val="a1"} }, B = { {ik="B", val="b1"}, {ik="B", val="b2"} } })
+	---@source Data/Scripts/Utils/TableUtils.lua:321
 	---@generic K: kcd2def*luakey
 	---@generic V: kcd2def*luakey
 	---@nodiscard
 	---@type fun(array:{[K]: V}, indexKey: K): {[V]: {[K]: V}}
 	IndexArrayOfObjects = ...;
+	---@source Data/Scripts/Utils/TableUtils.lua:232
 	---@nodiscard
 	---@type fun(t: table): boolean
 	IsEmpty = ...;
 	--- Returns an index on which the provided value is stored in the provided table.
+	---@source Data/Scripts/Utils/TableUtils.lua:3
 	---@generic K: kcd2def*luakey
 	---@generic V: any
 	---@nodiscard
 	---@type fun(t: {[K]: V}, val: V): K?
 	Lookup = ...;
-	---@deprecated
-	---@type fun(typeName: string, override: kcd2def*tablelike?)
+	---@source Data/Scripts/Utils/TableUtils.lua:363
+	---@nodiscard
+	---@type fun(typeName: string, override: kcd2def*tablelike?): table
 	MakeFromType = ...;
 	--- Merges tables from varargs (starting from the second) to the first table without overwriting fields.
 	-- Example: table.Merge(A, B, C) = table.Merge(table.Merge(A, B), C)
 	-- Handles recursive tables
+	---@source Data/Scripts/Utils/TableUtils.lua:78
 	---@generic K: kcd2def*luakey
 	---@generic V: any
 	---@type fun(target: {[K]: V}, ...: {[K]: V}): {[K]: V}
 	Merge = ...;
 	--- Returns the first element in a sequence, and removes it from the sequence.
+	---@source Data/Scripts/Utils/TableUtils.lua:215
 	---@generic V: any
 	---@type fun(t: V[]): V
 	PopFirst = ...;
 	--- Returns the last element in a sequence, and removes it from the sequence.
+	---@source Data/Scripts/Utils/TableUtils.lua:224
 	---@generic V: any
 	---@type fun(t: V[]): V
 	PopLast = ...;
 	--- Reindex first level Lua Table (indexed from 1) to Modular Behavior Tree array (indexed from 0)
 	-- No need to make this recursice since MBT does not support 2d arrays
+	---@source Data/Scripts/Utils/TableUtils.lua:346
 	---@generic V: any
 	---@type fun(array: V[]): {[kcd2def*cppindex]: V}
 	ReindexTableToMBT = ...;
 	--- Removes a value contained in the table.
+	---@source Data/Scripts/Utils/TableUtils.lua:204
 	---@generic V: any
 	---@type fun(t: {[kcd2def*luakey]:V}, val: V): nil
 	RemoveValue = ...;
 	--- Removes all the values in the table that meet the provided predicate.
+	---@source Data/Scripts/Utils/TableUtils.lua:177
 	---@generic K: kcd2def*luakey
 	---@generic V: any
 	---@type fun(t: {[K]:V}, pred: fun(v: V): boolean?): nil
 	RemoveWithPredicate = ...;
+	---@source Data/Scripts/Utils/TableUtils.lua:378
 	---@generic V: any
 	---@nodiscard
 	---@type fun(k: integer, list: V[]): V[]
 	ReservoirSample = ...;
 	--- Picks random element from list, considering only valid elements.
 	-- Valid are those elements, for which callback(element,callbackParams) is not false
+	---@source Data/Scripts/Utils/TableUtils.lua:397
 	---@generic V: any
 	---@generic P: any
 	---@nodiscard
 	---@type fun(list: V[], callback: fun(element: V, callbackParams: P): boolean?, callbackParams: P): V?
 	RollingSample = ...;
+	---@source Data/Scripts/Utils/TableUtils.lua:268
 	---@generic K: kcd2def*luakey
 	---@generic V: any
 	---@nodiscard
@@ -219,21 +256,25 @@ table = {
 	Select = ...;
 	--- Returns a value in the provided
 	-- table that meets the provided predicate.
+	---@source Data/Scripts/Utils/TableUtils.lua:142
 	---@generic V: any
 	---@nodiscard
 	---@type fun(t: {[kcd2def*luakey]:V}, pred: (fun(V): boolean?)): V?
 	SelectOne = ...;
 	--- Returns a sequence which contains all the values
 	-- in the provided sequence that meet the provided predicate.
+	---@source Data/Scripts/Utils/TableUtils.lua:128
 	---@generic V: any
 	---@nodiscard
 	---@type fun(t: V[], pred: (fun(V): boolean?)): V[]
 	SelectValues = ...;
+	---@source Data/Scripts/Utils/TableUtils.lua:29
 	---@generic K: kcd2def*luakey
 	---@generic V: any
 	---@nodiscard
 	---@type fun(obj: {[K]:V}): copy: {[K]:V}
 	ShallowCopy = ...;
+	---@source Data/Scripts/Utils/TableUtils.lua:336
 	---@type fun(list: any[]): nil
 	Shuffle = ...;
 	--- Sorts simple indexed array of objects (tables with string keys)
