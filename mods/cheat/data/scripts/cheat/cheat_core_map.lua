5e28b95cec9b18b2807f3668449cd0a515ce0df9
pryans <pryans@localhost>
2025-02-15 19:23:42 -0500

    refactor core map

--- mods/cheat/data/scripts/cheat/cheat_core_map.lua
+++ mods/cheat/data/scripts/cheat/cheat_core_map.lua
@@ -1,10 +1,10 @@
 -- ============================================================================
 -- cheat_reveal_map
 -- ============================================================================
-Cheat:createCommandLegacy("cheat_reveal_map", "Cheat:cheat_reveal_map()", nil,
+Cheat:createCommand("cheat_reveal_map", nil,
     "Add a perk to reveal the entire map.",
     "Reveal the entire map", "cheat_reveal_map")
-function Cheat:cheat_reveal_map()
+function Cheat:cheat_reveal_map(c)
     player.soul:AddPerk("34e03c47-de53-482f-b3f5-555e7e36d70c")
     Cheat:logInfo("Map revealed.")
     return true
